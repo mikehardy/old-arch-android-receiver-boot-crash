c59f82c5d4b01f04201cabe8e3974e4fe2eb10b4
Mike Hardy <github@mikehardy.net>
2024-11-18 13:29:23 -0500

    attempt to reproduce 47570 with minimal reproducer from 47592
    - add notifee package, new arch, add onBackgroundEvent handler

--- ReproducerApp/index.js
+++ ReproducerApp/index.js
@@ -3,6 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
+import notifee from '@notifee/react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
@@ -11,6 +12,22 @@ AppRegistry.registerHeadlessTask('ExampleHeadlessTask', () => async taskData => 
     'ExampleHeadlessTask JS executing. Received taskData: ' +
       JSON.stringify(taskData),
   );
+      // Display a notification
+      await notifee.displayNotification({
+        title: 'Notification Title',
+        body: 'Main body content of the notification',
+        android: {
+            channelId: "misc",
+            // pressAction is needed if you want the notification to open the app when pressed
+            pressAction: {
+                id: 'default',
+            },
+        },
+    });
+});
+
+notifee.onBackgroundEvent(async event => {
+  console.log('notifee.onBackgroundEvent with event: ' + JSON.stringify(event));
 });
 
 AppRegistry.registerComponent(appName, () => App);

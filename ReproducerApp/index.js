0c3a0159229a075592ec1e7be451ec9b7e8bb12c
Mike Hardy <github@mikehardy.net>
2024-11-18 12:57:50 -0500

    fix: correctly conform to TaskProvider API
    registration should be of a function that returns a Task, and the Task
    is an async function that does stuff
    
    so should have been () => async () => {} not just async () => {}

--- ReproducerApp/index.js
+++ ReproducerApp/index.js
@@ -6,7 +6,7 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
-AppRegistry.registerHeadlessTask('ExampleHeadlessTask', async taskData => {
+AppRegistry.registerHeadlessTask('ExampleHeadlessTask', () => async taskData => {
   console.log(
     'ExampleHeadlessTask JS executing. Received taskData: ' +
       JSON.stringify(taskData),
